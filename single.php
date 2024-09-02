19a2b9e80dbadc9bbe19221d0f37bff41b212846
Sofie <soffis02@easv365.dk>
2024-09-02 15:02:27 +0200

    php filer

---
+++ single.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<main>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+        <?php comments_template(); ?>
+    <?php endwhile; endif; ?>
+</main>
+<?php get_footer(); ?>
